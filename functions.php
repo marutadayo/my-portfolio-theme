a7b9a9fa3880f392ec93a67fca35c7301f12c961
Yuya Ishikawa <vkxsty_1k@icloud.com>
2026-06-04 07:19:37 +0900

    docs: 学習で利用したコメントアウトを削除

--- functions.php
+++ functions.php
@@ -11,11 +11,10 @@ function my_portfolio_scripts() {
   // リセットCSS
   wp_enqueue_style('reset-css', get_template_directory_uri() . '/css/reset.css', array(), '1.0.0');
   
-  // メインCSS（Live Sass Compilerで書き出されたもの）
-  // filemtime() を使うと、CSSを保存するたびにキャッシュがクリアされる実務テクニックです
+  // メインCSS
   wp_enqueue_style('main-style', get_template_directory_uri() . '/css/style.css', array('reset-css'), filemtime(get_theme_file_path('/css/style.css')));
 
-  // メインJS（フッターで読み込むために第5引数を true に設定）
+  // メインJS
   wp_enqueue_script('main-script', get_template_directory_uri() . '/js/script.js', array(), filemtime(get_theme_file_path('/js/script.js')), true);
 }
 add_action('wp_enqueue_scripts', 'my_portfolio_scripts');
--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 
 {
   document.addEventListener('DOMContentLoaded', () => {
-    //必ず必要ではないが画面読み込み時にイベント処理など入れる場合はお作法として'DOMContentLoaded'を入れておく。
-    //ブラウザがHTMLを読み終わる前にJavaScriptが走り出すのを防ぐため。HTMLのパース（解析）が全部終わったから動いてOKともいう。
   
     // SP or tablet Hamburger menu
     const $navBtn = document.querySelector('.js-nav-btn');
@@ -15,12 +13,7 @@
       $navBar.forEach(ber => {
         ber.classList.remove('bar-active');
       });
-  
-      // ?.（オプショナルチェーン）if文と同じだが分岐先がなかった場合に使う
-      // もしなかったらスルーするだけ
-      // 分岐させたいときはif else を使うか 代わりのデータをセットしたい時は ?? も使える。
-      // 書かなくても動くのになぜ書くか、「防衛的プログラミング」保険をかける
-      //「予期せぬ変更があっても、エラーで画面全体がフリーズするのだけは防ぐ」
+
       $navBtn.previousElementSibling?.classList.remove('cover');
       $navBtn.nextElementSibling?.classList.remove('nav-open');
     }
@@ -28,8 +21,6 @@
     $navBtn.addEventListener('click', function () {
   
       this.classList.toggle('close-btn');
-      //子要素の .nav-berはjQueryではfind等で子要素を指定できるがvanillaではquerySelectorAllなど他にも色々ある
-      //querySelectorAllは配列で返す。配列の中身を一つずつ処理する必要があるのでforEachを入れる。
       const $navBar = this.querySelectorAll('.nav-ber');
       $navBar.forEach(ber => {
         ber.classList.toggle('bar-active');
@@ -55,8 +46,7 @@
   
     //main-visulal animations
     const $fadeElements = document.querySelectorAll('.js-nav-btn, .js-site-name, .js-header-nav');
-    //これもさっきのハンバーガーメニューと同じだがquerySelectorAllは親兄弟子関係なくクラスを指定し配列にできる。
-    //逆にカンマで区切らなければ子孫要素まで取得してしまう。なので後はforEachで一つずつ取り出して処理していくだけ。
+
     $fadeElements.forEach(navItem => {
       navItem.classList.add('header-active');
     });
@@ -100,8 +90,8 @@
     //modal click triggers
     const $modalTriggers = document.querySelectorAll('.js-modal-trigger');
     const $bgOverlay = document.querySelector('#js-bg-overlay');
-    const $html = document.documentElement; //  htmlタグを取得
-    const $body = document.body;            //  bodyタグを取得
+    const $html = document.documentElement;
+    const $body = document.body;
   
     $modalTriggers.forEach(trigger => {
       trigger.addEventListener('click', () => {
@@ -116,7 +106,7 @@
       });
     });
   
-    // 【準備】モーダルを閉じる
+    // モーダルを閉じる
     const closeModal = () => {
       const $openModal = document.querySelector('.modal-open');
       if ($openModal) {
@@ -128,7 +118,7 @@
       $body.classList.remove('page-stop');
     };
   
-    // 【実行】モーダル本体（全画面）の「どこか」をクリックした時
+    // モーダル本体（全画面）の「どこか」をクリックした時
     const $modals = document.querySelectorAll('.js-modal');
   
     $modals.forEach(modal => {
@@ -157,26 +147,21 @@
     });
   
     // スクロールでフワッと表示させるアニメーション
-    // 1. 監視対象（.c-js-fade）のリストを作る
     const $fadeElem = document.querySelectorAll('.c-js-fade');
     const options = {
-      // 画面の下から「-100px」内側に入った時を交差検知にする
-      //rootは交差監視をする枠のような要素
       rootMrtgin: '0px 0px -100px 0px'
     }
   
-    // 2. 交差監視API（Observer）のルールを決める　
+    // 交差監視API
     const fadeObserver = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
-        // isIntersecting（イズ・インターセクティング ＝ 交差しているか？）
         if (entry.isIntersecting) {
           entry.target.classList.add('fade-in');
           fadeObserver.unobserve(entry.target);
         }
       });
-      // 見張らせるとき、第2引数に「指示書（options）」を渡す
     }, options);
-    // 3. 全員を一斉に監視スタート
+    // 全員を一斉に監視スタート
     $fadeElem.forEach(Elem => {
       fadeObserver.observe(Elem);
     });
